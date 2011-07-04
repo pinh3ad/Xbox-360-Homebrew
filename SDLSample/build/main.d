268d328de0ab3b9a9c71bb9f97bb3d7aed62f7a3
UNIX <kuckti15@gmail.com>
2011-07-04 13:48:09 -0400

    Added xenon-SDL sample!

---
+++ SDLSample/build/main.d
@@ -0,0 +1,91 @@
+main.o: /home/tiffany/Xbox-360-Homebrew/SDLSample/source/main.c \
+ /usr/local/xenon/usr/include/SDL/SDL.h \
+ /usr/local/xenon/usr/include/SDL/SDL_main.h \
+ /usr/local/xenon/usr/include/SDL/SDL_stdinc.h \
+ /usr/local/xenon/usr/include/SDL/SDL_config.h \
+ /usr/local/xenon/usr/include/SDL/SDL_platform.h \
+ /usr/local/xenon/usr/include/xetypes.h \
+ /usr/local/xenon/usr/include/SDL/begin_code.h \
+ /usr/local/xenon/usr/include/SDL/close_code.h \
+ /usr/local/xenon/usr/include/SDL/SDL_audio.h \
+ /usr/local/xenon/usr/include/SDL/SDL_error.h \
+ /usr/local/xenon/usr/include/SDL/SDL_endian.h \
+ /usr/local/xenon/usr/include/SDL/SDL_mutex.h \
+ /usr/local/xenon/usr/include/SDL/SDL_thread.h \
+ /usr/local/xenon/usr/include/SDL/SDL_rwops.h \
+ /usr/local/xenon/usr/include/SDL/SDL_cdrom.h \
+ /usr/local/xenon/usr/include/SDL/SDL_cpuinfo.h \
+ /usr/local/xenon/usr/include/SDL/SDL_events.h \
+ /usr/local/xenon/usr/include/SDL/SDL_active.h \
+ /usr/local/xenon/usr/include/SDL/SDL_keyboard.h \
+ /usr/local/xenon/usr/include/SDL/SDL_keysym.h \
+ /usr/local/xenon/usr/include/SDL/SDL_mouse.h \
+ /usr/local/xenon/usr/include/SDL/SDL_video.h \
+ /usr/local/xenon/usr/include/SDL/SDL_joystick.h \
+ /usr/local/xenon/usr/include/SDL/SDL_quit.h \
+ /usr/local/xenon/usr/include/SDL/SDL_loadso.h \
+ /usr/local/xenon/usr/include/SDL/SDL_timer.h \
+ /usr/local/xenon/usr/include/SDL/SDL_version.h \
+ /home/tiffany/Xbox-360-Homebrew/SDLSample/source/video_init.h \
+ /usr/local/xenon/usr/include/xenos/xenos.h \
+ /usr/local/xenon/usr/include/console/console.h
+
+/usr/local/xenon/usr/include/SDL/SDL.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_main.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_stdinc.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_config.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_platform.h:
+
+/usr/local/xenon/usr/include/xetypes.h:
+
+/usr/local/xenon/usr/include/SDL/begin_code.h:
+
+/usr/local/xenon/usr/include/SDL/close_code.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_audio.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_error.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_endian.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_mutex.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_thread.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_rwops.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_cdrom.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_cpuinfo.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_events.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_active.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_keyboard.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_keysym.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_mouse.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_video.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_joystick.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_quit.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_loadso.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_timer.h:
+
+/usr/local/xenon/usr/include/SDL/SDL_version.h:
+
+/home/tiffany/Xbox-360-Homebrew/SDLSample/source/video_init.h:
+
+/usr/local/xenon/usr/include/xenos/xenos.h:
+
+/usr/local/xenon/usr/include/console/console.h:
