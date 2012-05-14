e223895d56ecf85171b54ca886488e99b2b4e7f2
Tyler Kuck <kuckti15@gmail.com>
2012-05-13 23:44:58 -0400

    Added magic number to check against

---
+++ RemoteExecution/build/main.d
@@ -0,0 +1,20 @@
+main.o: \
+ /home/tyler/Desktop/Xbox-360-Homebrew/RemoteExecution/source/main.c \
+ /home/tyler/Desktop/Xbox-360-Homebrew/RemoteExecution/source/video_init.h \
+ /usr/local/xenon/usr/include/xenos/xenos.h \
+ /usr/local/xenon/usr/include/console/console.h \
+ /usr/local/xenon/usr/include/xetypes.h \
+ /usr/local/xenon/usr/include/xenon_uart/xenon_uart.h \
+ /usr/local/xenon/usr/include/elf/elf.h
+
+/home/tyler/Desktop/Xbox-360-Homebrew/RemoteExecution/source/video_init.h:
+
+/usr/local/xenon/usr/include/xenos/xenos.h:
+
+/usr/local/xenon/usr/include/console/console.h:
+
+/usr/local/xenon/usr/include/xetypes.h:
+
+/usr/local/xenon/usr/include/xenon_uart/xenon_uart.h:
+
+/usr/local/xenon/usr/include/elf/elf.h:
