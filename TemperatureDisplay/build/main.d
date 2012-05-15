1e84a8e8a3790691a86cc40d6939d2f2144cd802
Tyler Kuck <kuckti15@gmail.com>
2012-05-15 01:16:18 -0400

    Fixed misspelled include file, de-initialized USB, not needed in this code.

--- TemperatureDisplay/build/main.d
+++ TemperatureDisplay/build/main.d
@@ -1,19 +1,20 @@
-main.o: /home/tiffany/libxenon/temps/source/main.c \
+main.o: \
+ /home/tyler/Desktop/Xbox-360-Homebrew/TemperatureDisplay/source/main.c \
  /usr/local/xenon/usr/include/console/console.h \
+ /usr/local/xenon/usr/include/xetypes.h \
  /usr/local/xenon/usr/include/xenos/xenos.h \
  /usr/local/xenon/usr/include/xenos/xe.h \
- /usr/local/xenon/usr/include/xetypes.h \
- /usr/local/xenon/usr/include/nocfe/lib_malloc.h \
- /home/tiffany/libxenon/temps/source/video_init.h
+ /usr/local/xenon/usr/include/xenon_smc/xenon_smc.h \
+ /home/tyler/Desktop/Xbox-360-Homebrew/TemperatureDisplay/source/video_init.h
 
 /usr/local/xenon/usr/include/console/console.h:
+
+/usr/local/xenon/usr/include/xetypes.h:
 
 /usr/local/xenon/usr/include/xenos/xenos.h:
 
 /usr/local/xenon/usr/include/xenos/xe.h:
 
-/usr/local/xenon/usr/include/xetypes.h:
+/usr/local/xenon/usr/include/xenon_smc/xenon_smc.h:
 
-/usr/local/xenon/usr/include/nocfe/lib_malloc.h:
-
-/home/tiffany/libxenon/temps/source/video_init.h:
+/home/tyler/Desktop/Xbox-360-Homebrew/TemperatureDisplay/source/video_init.h:
