a367175af3e22b41ca33e27dbbbacfaed686e2c1
Tyler Kuck <kuckti15@gmail.com>
2012-05-16 05:57:47 -0400

    Setup FILE type and fread() to read uploaded binary data once first 4 bytes received are verified. Added verbose comments, cleaned up format to make more readable.

--- RemoteExecution/source/main.c
+++ RemoteExecution/source/main.c
@@ -15,10 +15,10 @@
 
 // An array of bytes containing the ELF magic number
 // TODO: Change this to {'E', 'L', 'F'}; ?
-unsigned char ELF_MAGIC[5] = {0x7f, 0x45, 0x4c, 0x46};
+unsigned char ELF_MAGIC[4] = {0x7f, 0x45, 0x4c, 0x46};
 
 // Point at a specific address to store incoming data
-//unsigned char *elfAddress = (unsigned char *) 0xA00000;
+unsigned char *elfAddress = (unsigned char *) 0xA00000;
 
 int main(){
 	videoInit();
@@ -43,11 +43,38 @@ int main(){
 	if(kbhit()){
 		// Read from UART
 		unsigned char data = getch();
+		// Create FILE type for incoming data. We will use this later.
+		FILE * elfFile = fopen(data, "rb");
 
 		while(data){
+			/* If size of "data" is 4 bytes, cross check those 4 bytes
+			   with the ELF_MAGIC number array to see if what we are
+			   reading is an ELF. If yes, continue loop, if else, break.
+			*/
+			if(sizeof(data) == 0x4){
+				if(strcmp(data, ELF_MAGIC));
+					continue;
+			}
+			else{
+				break;
+			}
 
-			if(sizeof(data) == 0x4) if(strcmp(data, ELF_MAGIC)) continue;
-			else break;
+			/* If we pass the condition of the if above, execute fread()
+			   on data, to our memory pointer we are storing the ELF to.
+
+			   elfAddress: Specific memory address (0xA00000) to store ELF.
+			   dataSize: Size of each data element (0x1)
+			   dataElements: Number of elements to read (Needs moar magic)
+			   elfFile: FILE type set to "data", the actual binary data to be stored
+			*/
+			int dataSize = 0x1;
+
+			/* TODO: Need to add magic here, if I remember there is and ELF section
+			   for file size.
+			*/
+			int dataElements = sizeof(data);
+
+			fread(elfAddress, dataSize, dataElements, elfFile);
 		}
 	}
 
