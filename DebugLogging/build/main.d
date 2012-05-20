6fc1f027a263e21ae8a84e4822aa08c9c6695b60
Tyler Kuck <kuckti15@gmail.com>
2012-05-20 07:28:51 -0400

    Fixed to work with libfat.

--- DebugLogging/build/main.d
+++ DebugLogging/build/main.d
@@ -1,10 +1,22 @@
-main.o: /home/tiffany/Xbox-360-Homebrew/DebugLogging/source/main.c \
- /home/tiffany/Xbox-360-Homebrew/DebugLogging/source/video_init.h \
+main.o: /home/tyler/Desktop/Xbox-360-Homebrew/DebugLogging/source/main.c \
+ /home/tyler/Desktop/Xbox-360-Homebrew/DebugLogging/source/video_init.h \
  /usr/local/xenon/usr/include/xenos/xenos.h \
- /usr/local/xenon/usr/include/console/console.h
+ /usr/local/xenon/usr/include/console/console.h \
+ /usr/local/xenon/usr/include/xetypes.h \
+ /usr/local/xenon/usr/include/usb/usbmain.h \
+ /usr/local/xenon/usr/include/libfat/fat.h \
+ /usr/local/xenon/usr/include/diskio/disc_io.h
 
-/home/tiffany/Xbox-360-Homebrew/DebugLogging/source/video_init.h:
+/home/tyler/Desktop/Xbox-360-Homebrew/DebugLogging/source/video_init.h:
 
 /usr/local/xenon/usr/include/xenos/xenos.h:
 
 /usr/local/xenon/usr/include/console/console.h:
+
+/usr/local/xenon/usr/include/xetypes.h:
+
+/usr/local/xenon/usr/include/usb/usbmain.h:
+
+/usr/local/xenon/usr/include/libfat/fat.h:
+
+/usr/local/xenon/usr/include/diskio/disc_io.h:
